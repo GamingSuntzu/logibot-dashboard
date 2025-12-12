3f9734b277d1c7369f450e250237960c696680a6
Felix Gideon <felix.gideon3@gmail.com>
2025-12-12 14:46:46 +0700

    trying middleware for endpoints

--- src/app/api/hitl/[...catchall]/route.ts
+++ src/app/api/hitl/[...catchall]/route.ts
@@ -1,26 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 
+type Params = {
+  catchall: string[];
+};
+
 export async function GET(
   req: NextRequest,
-  context: { params: { catchall: string[] } }
+  context: { params: Promise<Params> }
 ) {
+  const { catchall } = await context.params;
+
   console.log("⚠️ HITL CATCH-ALL GET");
-  console.log("Path:", context.params.catchall);
+  console.log("Path:", catchall);
 
   return NextResponse.json({ ok: true });
 }
 
 export async function POST(
   req: NextRequest,
-  context: { params: { catchall: string[] } }
+  context: { params: Promise<Params> }
 ) {
+  const { catchall } = await context.params;
+
   let body = null;
   try {
     body = await req.json();
   } catch {}
 
   console.log("⚠️ HITL CATCH-ALL POST");
-  console.log("Path:", context.params.catchall);
+  console.log("Path:", catchall);
   console.log("Body:", body);
 
   return NextResponse.json({ ok: true });
